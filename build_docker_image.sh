73226853b829468fc40cb4a856979640957a9acf
andysturrock <github@sturrock.org>
2015-04-03 09:55:47 +0100

    Added command to push container back to Docker repo

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -7,3 +7,5 @@ cp ${WORKSPACE}/target/jboss_docker.war ${WORKSPACE}/Dockerfiles
 docker build -t andysturrock/centos:jboss-eap-helloworld ${WORKSPACE}/Dockerfiles
 
 rm ${WORKSPACE}/Dockerfiles/jboss_docker.war
+
+docker push andysturrock/centos:jboss-eap-helloworld
